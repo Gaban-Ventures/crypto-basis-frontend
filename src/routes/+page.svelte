c57dc6fa6e6b87b09de70471004b693cbc5ca8fb
Leon Gaban <leongaban@gmail.com>
2024-12-15 10:07:42 -0600

    updated main page, added styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import '@/app.css';
+</script>
+
+<h1 class="space-grotesk-900">CRYPTO BASIS</h1>
+<h3 class="space-grotesk-300">Track your cost basis and P & L</h3>
+
+<style>
+  .space-grotesk-900 {
+		font-family: 'Space Grotesk', sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 900;
+		font-style: normal;
+	}
+	.space-grotesk-300 {
+		font-family: 'Space Grotesk', sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 300;
+		font-style: normal;
+	}
+</style>
