d51b656e2a952f5a74040000b3085e6eadce2423
Leon Gaban <leongaban@gmail.com>
2024-12-15 10:52:58 -0600

    created Header component and new css directory

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  let { title }: { title: string } = $props();
+</script>
+
+<h1 class="space-grotesk-900">{title.toLocaleUpperCase()}</h1>
+<h3 class="space-grotesk-300">Track your cost basis and P & L</h3>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,8 @@
-<script>
-  import '@/app.css';
+<script lang="ts">
+  import '@/css/index.css';
+  import Header from '$lib/components/Header.svelte';
+
+  let title = 'Crypto Basis';
 </script>
 
-<h1 class="space-grotesk-900">CRYPTO BASIS</h1>
-<h3 class="space-grotesk-300">Track your cost basis and P & L</h3>
-
-<style>
-  .space-grotesk-900 {
-		font-family: 'Space Grotesk', sans-serif;
-		font-optical-sizing: auto;
-		font-weight: 900;
-		font-style: normal;
-	}
-	.space-grotesk-300 {
-		font-family: 'Space Grotesk', sans-serif;
-		font-optical-sizing: auto;
-		font-weight: 300;
-		font-style: normal;
-	}
-</style>
+<Header title={title}/>
